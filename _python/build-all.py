1aafc650b37c3d0a810e3ce27851a12d803d387f
Henry Moon <hcmoon92@hotmail.com>
2024-12-01 21:58:15 +0900

    csts updated

--- _python/build-all.py
+++ _python/build-all.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 
 #subprocess.run(['python', 'make_tags.py'])
-subprocess.run(['python', 'copy_docs_to_posts.py'])
+#subprocess.run(['python', 'copy_docs_to_posts.py'])
+#subprocess.run(['python', 'copy_posts_to_docs.py'])
 subprocess.run(['python', 'make_tags_url.py'])
 #subprocess.run(['python', 'make_category.py'])
 subprocess.run(['python', 'make_categories_url.py'])
